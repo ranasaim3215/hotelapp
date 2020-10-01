e970b3a80c2b061b7945dc12c736f373bb79cf66
ranasaim3215 <aliusama321504@gmail.com>
2020-10-01 17:18:57 +0500

    your message

---
+++ property/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Reserve , Property
+
+
+class ReserveForm(forms.ModelForm):
+    class Meta : 
+        model = Reserve
+        fields = '__all__'
+
+        
